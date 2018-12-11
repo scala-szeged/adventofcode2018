ef6823f7624cc9e81efc6f0a0c5038e166d78519
jseteny <jseteny@gmail.com>
2018-12-11 19:21:37 +0100

    My Day01 solution

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -1,12 +1,25 @@
+import scala.annotation.tailrec
+
+
 object Day01 {
 
-  def task1(ints: List[Int]): Int = ints.sum
+  def task1(changes: List[Int]): Int = changes.sum
 
-  def task2(ints: List[Int]): Int = Stream.iterate((0, 0, List.empty[Int])){
-    case (idx, freq, visitedFreqs) =>
-      (idx + 1, freq + ints(idx % ints.size), freq :: visitedFreqs)
-  }.flatMap{
-    case (_, freq, visitedFreqs) =>
-      if (visitedFreqs contains freq) Some(freq) else None
-  }.head
+  def task2(changes: List[Int]): Int = {
+
+    @tailrec
+    def reachedTwice(remaining: List[Int], frequencies: Set[Int] = Set(0), lastFrequency: Int = 0): Int = remaining match {
+      case head :: tail =>
+        val f = lastFrequency + head
+        if (frequencies.contains(f))
+          f
+        else
+          reachedTwice(tail, frequencies + f, f)
+
+      case Nil =>
+        reachedTwice(changes, frequencies, lastFrequency)
+    }
+
+    reachedTwice(changes)
+  }
 }
